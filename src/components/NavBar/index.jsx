1804b4853170889dba3ecd72f9fb5bcb0a12fbf6
José Parra Zúñiga <parrazunigajosedaniel87@gmail.com>
2022-10-29 13:34:09 -0600

    Agregado CartWidget, ItemListContainer y Greeting

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import CartWidget from "../CartWidget";
 
 export const NavBar = () => {
     return (
         <div className="container">
             <nav className="nav">
                 <div className="nav_brand">
-                    <a className="nav_link" href="#">MiMarca</a>
+                    <a className="nav_link" href="https://www.google.com/">MiMarca</a>
                 </div>
                 <ul className="nav_list">
                     <li>
-                        <a className="nav_link" href="#">Categoría 1</a>
+                        <a className="nav_link" href="https://www.google.com/">Categoría 1</a>
                     </li>
                     <li>
-                        <a className="nav_link" href="#">Categoría 2</a>
+                        <a className="nav_link" href="https://www.google.com/">Categoría 2</a>
                     </li>
                     <li>
-                        <a className="nav_link" href="#">Carrito</a>
+                        <a className="nav_link" href="https://www.google.com/">
+                            <CartWidget />
+                        </a>
                     </li>
                 </ul>
             </nav>
