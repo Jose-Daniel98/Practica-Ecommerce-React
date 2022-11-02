ae59b5ba9f68c536b528564bae7889bc31c2177d
José Parra Zúñiga <parrazunigajosedaniel87@gmail.com>
2022-11-02 09:50:04 -0600

    PreEntrega1 + JoseParraZúñiga

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import CartWidget from "../CartWidget";
+import "./navBar.css";
+
 
 export const NavBar = () => {
     return (
         <div className="container">
             <nav className="nav">
                 <div className="nav_brand">
-                    <a className="nav_link" href="https://www.google.com/">MiMarca</a>
+                    <a className="nav_link" href="https://www.google.com/">🏠</a>
                 </div>
                 <ul className="nav_list">
                     <li>
-                        <a className="nav_link" href="https://www.google.com/">Categoría 1</a>
+                        <a className="nav_link" href="https://www.google.com/">Nosotros</a>
                     </li>
                     <li>
-                        <a className="nav_link" href="https://www.google.com/">Categoría 2</a>
+                        <a className="nav_link" href="https://www.google.com/">Productos</a>
                     </li>
                     <li>
                         <a className="nav_link" href="https://www.google.com/">
